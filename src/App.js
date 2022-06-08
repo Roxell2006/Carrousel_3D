323d2b92bb938dfe16dbeccb376ad240b6488b24
roxell2006 <romualdhansen2006@gmail.com>
2022-06-08 23:08:33 +0200

    carrousel 3d

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Carousel3D from './Carousel3D';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Carousel3D>
+        <span><img src="https://icons.iconarchive.com/icons/mattahan/ultrabuuf/512/Comics-Ignorant-Creature-icon.png"/></span>
+        <span><img src="https://icons.iconarchive.com/icons/mattahan/ultrabuuf/512/Comics-Hulkling-Sulking-icon.png"/></span>
+        <span><img src="https://icons.iconarchive.com/icons/mattahan/ultrabuuf/512/Comics-Logan-icon.png"/></span>
+        <span><img src="https://icons.iconarchive.com/icons/mattahan/ultrabuuf/512/Street-Fighter-Zangief-icon.png"/></span>
+        <span><img src="https://icons.iconarchive.com/icons/mattahan/ultrabuuf/512/Street-Fighter-Chun-Li-icon.png"/></span>
+        <span><img src="https://icons.iconarchive.com/icons/mattahan/ultrabuuf/512/Comics-Older-Superman-icon.png"/></span>
+        <span><img src="https://icons.iconarchive.com/icons/mattahan/ultrabuuf/512/Comics-Thor-icon.png"/></span>
+        <span><img src="https://icons.iconarchive.com/icons/mattahan/ultrabuuf/512/Street-Fighter-Sakura-Kasugano-icon.png"/></span>    
+    </Carousel3D>
   );
 }
 
